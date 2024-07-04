c959d07b09527a1ec4e85e2ec2ccff9a173eca54
giorgimemarnidze <158182170+giorgimemarnidze@users.noreply.github.com>
2024-07-04 17:37:44 +0400

    feat: complete header component

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -1,3 +1,4 @@
+import Input from '../Input/Input';
 import styles from './Header.module.scss';
 import Image from 'next/image';
 
@@ -9,7 +10,7 @@ export default ({icon} : Props) => {
 
     return(
         <div className={styles.container}>
-        
+            <Input />
             <Image src={icon} alt='image' width={56} height={56} className={styles.image} />
         </div>
     )
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
-import Header from "./components/Header/Header";
-'use client'
-import Input from "./components/Input/Input";
 'use client';
+import Header from "./components/Header/Header";
+import Input from "./components/Input/Input";
 import Card from "./components/Card/Card";
 import Heart from "./components/CardItems/CardHeart/Heart";
 import ItemsUnion from "./components/CardItems/ItemsUnion/ItemsUnion";
@@ -10,6 +9,7 @@ import "./page.module.scss";
 export default function Home() {
   return (
     <main>
+        
     </main>
   );
 }
