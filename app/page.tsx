91c92367c312b960edca7732a7bf66dcf5253213
giorgimemarnidze <158182170+giorgimemarnidze@users.noreply.github.com>
2024-07-04 16:10:45 +0400

    fix: image hover fixing

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+'use client';
+import Card from "./components/Card/Card";
+import Heart from "./components/CardItems/CardHeart/Heart";
+import ItemsUnion from "./components/CardItems/ItemsUnion/ItemsUnion";
 import "./page.module.scss";
 
 export default function Home() {
   return (
     <main>
-      
+      <Card header={"Song Card"} image={"/song-card.svg"} subtitle={"Anyma"} title={"Genesys II"} imageStyle={'normal'}  />
     </main>
   );
 }
