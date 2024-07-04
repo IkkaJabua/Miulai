7cac7c7cde12e78a0849c5d40546a987be05e765
giorgimemarnidze <158182170+giorgimemarnidze@users.noreply.github.com>
2024-07-04 17:09:45 +0400

    fix: search icon disappear from placeholder

--- app/components/Input/Input.tsx
+++ app/components/Input/Input.tsx
@@ -1,21 +1,12 @@
 import { useEffect, useState } from 'react';
 import styles from './Input.module.scss';
-import Image from 'next/image';
 
 
 export default () => {
-    const [showIcon, setShowIcon] = useState(true);
-
-    const showing = () => {
-        setShowIcon(!showIcon);
-    }
 
     return (
-        <div className={styles.container}  onFocus={showing} onBlur={showing}>
-          
-                {showIcon && <Image src={'./searchIconBlur.svg'} alt='image' width={24} height={24} className={styles.img} />}
-                <input type="text" placeholder='Artists,tracks,albums' className={styles.input} />
-
+        <div className={styles.container}>
+            <input type="text" placeholder='Artists,tracks,albums' className={styles.input} />
         </div>
     )
 }
