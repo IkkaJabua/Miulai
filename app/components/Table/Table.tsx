5817b50ae6bcbd1ae38276523cfcbc333c79cbe1
Sandro-Zosiashvili <zosiashvilisavle@gmail.com>
2024-07-29 13:23:43 +0400

    fix: delete

--- app/components/Table/Table.tsx
+++ app/components/Table/Table.tsx
@@ -1,123 +1,48 @@
-import { time } from 'console'
+import { Col, Row } from 'antd'
 import styles from './Table.module.scss'
-import Image from 'next/image'
-import Heart from '../CardItems/CardHeart/Heart';
-import HeartShapeBtn from '../heatShapeIcon/HeartShapeIcn';
+import Image from 'antd'
+import HeartShapeBtn from '../heatShapeIcon/HeartShapeIcn'
+import helper from './helper'
 
 
 
 export default () => {
 
-    const tableData = [
-        {
-            icon: '/table-icon.png',
-            title: 'Girls Are Fascinating',
-            author: 'By Anetha',
-            album: 'Mothearth',
-            time: '3:54',
-            id: 1
-        }, {
-            icon: '/table-icon2.svg',
-            title: 'Smash My Heart',
-            author: 'By Anetha',
-            album: 'Pink',
-            time: '3:54',
-            id: 2
-        }, {
-            icon: '/table-icon3.svg',
-            title: 'Blackbird',
-            author: 'By Anetha',
-            album: 'Cowboy Carter',
-            time: '3:54',
-            id: 3
-        }, {
-            icon: '/table-icon4.svg',
-            title: 'Human',
-            author: 'By Anetha',
-            album: 'Zaba',
-            time: '3:54',
-            id: 4
-        }, {
-            icon: '/table-icon5.svg',
-            title: 'Toes',
-            author: 'By Anetha',
-            album: 'Zaba',
-            time: '3:54',
-            id: 5
-        }, {
-            icon: '/table-icon6.svg',
-            title: 'Picture Of You',
-            author: 'By Anetha',
-            album: 'Genesys II',
-            time: '3:54',
-            id: 6
-        }, {
-            icon: '/table-icon7.svg',
-            title: 'End Of An Era',
-            author: 'By Anetha',
-            album: 'Radical Optimism',
-            time: '3:54',
-            id: 7
-        }, {
-            icon: '/table-icon8.svg',
-            title: 'Your Art',
-            author: 'By Anetha',
-            album: 'I Hear You',
-            time: '3:54',
-            id: 8
-        }, {
-            icon: '/table-icon9.svg',
-            title: 'Girls Are Fascinating',
-            author: 'By Anetha',
-            album: 'Poker Face',
-            time: '3:54',
-            id: 9
-        }, {
-            icon: '/table-icon10.svg',
-            title: 'The man',
-            author: 'By Anetha',
-            album: 'Lover',
-            time: '3:54',
-            id: 10
-        }, {
-            icon: '/table-icon11.svg',
-            title: 'So Fresh, So  Clean',
-            author: 'By Anetha',
-            album: 'Stankonia',
-            time: '3:54',
-            id: 11
-        }
-    ]
+    const [tableData] = helper
 
 
     return (
-        <div className={styles.musicTable}>
-            <div className={styles.tableHeader}>
-                <div className={styles.headerNumber}>#</div>
-                <div className={styles.headerSongname}>Song Name</div>
-                <div className={styles.headerAlbum}>Album</div>
-                <div className={styles.headerTime}>Time</div>
-            </div>
-            <div className={styles.tableBody}>
-                {tableData.map((item) => (
-                    <div className={styles.tableRow}>
-                        <div className={styles.cellNumber}>{item.id}</div>
-                        <div className={styles.cellSongname}>
-                            <Image src={item.icon} width={48} height={48} alt={item.title} />
-                            <div className={styles.fontGap}>
-                                <div className={styles.songTitle}>{item.title}</div>
-                                <div className={styles.songArtist}>{item.author}</div>
+        <div className={styles.mainContainer}>
+            <Row className={styles.container}>
+                <Col className={styles.cellNumber} span={1}>#</Col>
+                <Col className={styles.headerSongname} span={7}>Song Name</Col>
+                <Col className={styles.headerAlbum} span={6}>Album</Col>
+                <Col className={styles.headerTime} span={6}>Time</Col>
+                <Col className={styles.HeaderLike} span={3}></Col>
+            </Row>
+            {
+                tableData.map((item) => (
+                    <Row className={styles.musicTable} >
+                        <Col className={styles.cellId} span={1}>
+                            <div>{item.id}</div>
+                        </Col>
+                        <Col className={styles.cellName} span={7}>
+                            <div className={styles.cellSongname}>
+                                <img src={item.icon} width={48} height={48} alt={item.title} />
+                                <div className={styles.fontGap}>
+                                    <div className={styles.songTitle}>{item.title}</div>
+                                    <div className={styles.songArtist}>{item.author}</div>
+                                </div>
                             </div>
-                        </div>
-                        <div className={styles.cellAlbum}>{item.album}</div>
-                        <div className={styles.cellTime}>{item.time}</div>
-                        <div className={styles.cellFavorite}>
+                        </Col>
+                        <Col className={styles.cellAlbumName} span={6}>{item.album}</Col>
+                        <Col className={styles.cellTimeName} span={6}>{item.time}</Col>
+                        <Col className={styles.cellLike} span={3}>
                             <HeartShapeBtn isActive={false} isDisabled={false} onClick={() => { console.log("button clicked") }} />
-                        </div>
-                    </div>
-                ))}
-            </div>
+                        </Col>
+                    </Row>
+                ))
+            }
         </div>
-    );
-};
-
+    )
+}
--- app/components/Table/helper.tsx
+++ app/components/Table/helper.tsx
@@ -1,9 +1,4 @@
 
-import { Col, Row } from 'antd'
-import styles from './satesto.module.scss'
-import Image from 'antd'
-import HeartShapeBtn from '../heatShapeIcon/HeartShapeIcn'
-
 
 
 export default () => {
@@ -91,38 +86,5 @@ export default () => {
 
 
 
-    return (
-        <div className={styles.mainContainer}>
-            <Row className={styles.container}>
-                <Col className={styles.cellNumber} span={1}>#</Col>
-                <Col className={styles.headerSongname} span={4}>Song Name</Col>
-                <Col className={styles.headerAlbum} span={6}>Album</Col>
-                <Col className={styles.headerTime} span={4}>Time</Col>
-                {/* <Col className={styles.HeaderLike} span={3}></Col> */}
-            </Row>
-            {
-                tableData.map((item) => (
-                    <Row className={styles.musicTable} >
-                        <Col className={styles.cellId} span={1}>
-                            <div>{item.id}</div>
-                        </Col>
-                        <Col className={styles.cellName} span={4}>
-                            <div className={styles.cellSongname}>
-                                <img src={item.icon} width={48} height={48} alt={item.title} />
-                                <div className={styles.fontGap}>
-                                    <div className={styles.songTitle}>{item.title}</div>
-                                    <div className={styles.songArtist}>{item.author}</div>
-                                </div>
-                            </div>
-                        </Col>
-                        <Col className={styles.cellAlbumName} span={6}>{item.album}</Col>
-                        <Col className={styles.cellTimeName} span={4}>{item.time}</Col>
-                        <Col className={styles.cellLike} span={3}>
-                            <HeartShapeBtn isActive={false} isDisabled={false} onClick={() => { console.log("button clicked") }} />
-                        </Col>
-                    </Row>
-                ))
-            }
-        </div>
-    )
+    return {tableData}
 }
