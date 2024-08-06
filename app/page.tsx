95244fa55c542b83350640df15deda6a3bfc9302
Sandro-Zosiashvili <zosiashvilisavle@gmail.com>
2024-08-07 00:42:28 +0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,22 @@
+import Button from "./components/Button/Button";
+import Input from "./components/Input/Input";
+import Menu from "./components/Menu/Menu";
+import News from "./components/News/News";
+import "./page.module.scss";
+
+
+
+
+
+
+export default function Home() {
+    return (
+        <main>
+
+        </main>
+    );
+}
+
 
 
     
