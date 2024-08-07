e92c603f6fd3122d16ad3803afc827494402dfa5
Sandro-Zosiashvili <zosiashvilisavle@gmail.com>
2024-08-07 15:03:52 +0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Button from "./components/Button/Button";
 import Input from "./components/Input/Input";
 import Menu from "./components/Menu/Menu";
 import News from "./components/News/News";
+import UserPlaylist from "./components/UserPlaylist/UserPlaylist";
 import "./page.module.scss";
 
 
@@ -12,7 +13,7 @@ import "./page.module.scss";
 export default function Home() {
     return (
         <main>
-            
+            <UserPlaylist />
         </main>
     );
 }
