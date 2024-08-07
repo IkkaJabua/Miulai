6bf36b1a859b233a197215d6fe0a22e4445ae062
Sandro-Zosiashvili <zosiashvilisavle@gmail.com>
2024-08-07 14:28:10 +0400

    fix: comment

--- app/components/UserPlaylist/UserPlaylist.tsx
+++ app/components/UserPlaylist/UserPlaylist.tsx
@@ -7,8 +7,8 @@ export default () => {
 
     return (
         <div className={styles.container}>
-            <Image src={'./album.svg'} width={234}  height={251} alt='image'/>
-            <div className={styles.font}>Playlist name 1</div>
+            {/* <Image src={'./album.svg'} width={234}  height={251} alt='image'/>
+            <div className={styles.font}>Playlist name 1</div> */}
         </div>
     )
 }
