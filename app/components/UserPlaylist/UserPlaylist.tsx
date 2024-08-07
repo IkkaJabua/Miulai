023ecd97c11d8d77f5fbe7b1ff1464ffc20f49ce
Sandro-Zosiashvili <zosiashvilisavle@gmail.com>
2024-08-07 14:48:01 +0400

    Update UserPlaylist.tsx

--- app/components/UserPlaylist/UserPlaylist.tsx
+++ app/components/UserPlaylist/UserPlaylist.tsx
@@ -5,17 +5,11 @@ import Image from 'next/image'
 
 export default () => {
     
-
-
-
-
-
-
-    
     return (
         <div className={styles.container}>
             <Image src={'./album.svg'} width={234}  height={251} alt='image'/>
             <div className={styles.font}>Playlist name 1</div>
         </div>
+        
     )
 }
