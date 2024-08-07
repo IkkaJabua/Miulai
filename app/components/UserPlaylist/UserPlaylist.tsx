e70d7269538092a4055e74dbd4bc165c6fc894b0
Sandro-Zosiashvili <zosiashvilisavle@gmail.com>
2024-08-07 14:29:58 +0400

    Update UserPlaylist.tsx

--- app/components/UserPlaylist/UserPlaylist.tsx
+++ app/components/UserPlaylist/UserPlaylist.tsx
@@ -7,8 +7,10 @@ export default () => {
 
     return (
         <div className={styles.container}>
-            {/* <Image src={'./album.svg'} width={234}  height={251} alt='image'/>
-            <div className={styles.font}>Playlist name 1</div> */}
+            <Image src={'./album.svg'} width={234}  height={251} alt='image'/>
+            <div className={styles.font}>Playlist name 1</div>
+
+            
         </div>
     )
 }
