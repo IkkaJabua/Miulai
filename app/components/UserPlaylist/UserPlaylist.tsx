9411878b58d7749b9ef5329bb2891c570779e3ae
Sandro-Zosiashvili <zosiashvilisavle@gmail.com>
2024-08-08 17:20:41 +0400

    fix: problem

--- app/components/UserPlaylist/UserPlaylist.tsx
+++ app/components/UserPlaylist/UserPlaylist.tsx
@@ -10,19 +10,24 @@ export default () => {
     const playListData = [
         {
             title: 'Playlist name 1',
-            icon: 'album.svg'
+            icon: 'albumicon1.svg',
+            id: 1,
         }, {
             title: 'Playlist name 2',
-            icon: 'albumicon2.svg'
+            icon: 'albumicon2.svg',
+            id: 2,
         }, {
             title: 'Playlist name 3',
-            icon: 'albumicon3.svg'
+            icon: 'albumicon3.svg',
+            id: 3,
         }, {
             title: 'Playlist name 4',
-            icon: 'albumicon4.svg'
+            icon: 'albumicon4.svg',
+            id: 4,
         }, {
             title: 'Playlist name 5',
-            icon: 'albumicon5.svg'
+            icon: 'albumicon5.svg',
+            id: 5
         }
     ]
 
@@ -32,7 +37,19 @@ export default () => {
             {
                 playListData.map((item) =>
                     <div className={styles.container} >
-                        <Image src={`./icon/${item.icon}`} width={234} height={251} alt='image' />
+                        <div className={styles.hoveredImage}
+                            style={{
+                                backgroundImage: `linear-gradient(0deg, rgba(149, 146, 146, 0.2)0%, rgba(149, 146, 146, 0.2)100%), url('./icon/${item.icon}')`,
+                                backgroundSize: "cover",
+                                backgroundPosition: "center",
+                                backgroundRepeat: "no-repeat",
+                                width: "100%",
+                                height: "300px",
+                                borderRadius: '8px'
+
+                            }}
+                        >
+                        </div>
                         <div className={styles.font}>{item.title}</div>
                     </div>
                 )
