adbabbc8e5f05a9b27f1254a8061b95b2323856d
Sandro-Zosiashvili <zosiashvilisavle@gmail.com>
2024-08-08 16:39:56 +0400

    feat

--- app/components/UserPlaylist/UserPlaylist.tsx
+++ app/components/UserPlaylist/UserPlaylist.tsx
@@ -1,14 +1,43 @@
+'use client'
+import { title } from 'process'
 import styles from './UserPlaylist.module.scss'
 import Image from 'next/image'
 
 
 export default () => {
 
+
+    const playListData = [
+        {
+            title: 'Playlist name 1',
+            icon: 'album.svg'
+        }, {
+            title: 'Playlist name 2',
+            icon: 'albumicon2.svg'
+        }, {
+            title: 'Playlist name 3',
+            icon: 'albumicon3.svg'
+        }, {
+            title: 'Playlist name 4',
+            icon: 'albumicon4.svg'
+        }, {
+            title: 'Playlist name 5',
+            icon: 'albumicon5.svg'
+        }
+    ]
+
+
     return (
-        <div className={styles.container}>
-            <Image src={'./album.svg'} width={234} height={251} alt='image' />
-            <div className={styles.font}>Playlist name 1</div>
-        </div>
+        <>
+            {
+                playListData.map((item) =>
+                    <div className={styles.container} >
+                        <Image src={`./icon/${item.icon}`} width={234} height={251} alt='image' />
+                        <div className={styles.font}>{item.title}</div>
+                    </div>
+                )
+            }
+        </>
     )
 }
 
