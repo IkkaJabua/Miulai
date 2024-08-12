aa1768a3c1c7c353ec04d9b09877717464ab17aa
Sandro-Zosiashvili <zosiashvilisavle@gmail.com>
2024-08-13 01:32:37 +0300

    ff

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Button from "./components/Button/Button";
 import Input from "./components/Input/Input";
 import Menu from "./components/Menu/Menu";
 import News from "./components/News/News";
+import Table from "./components/Table/Table";
 import "./page.module.scss";
 
 
