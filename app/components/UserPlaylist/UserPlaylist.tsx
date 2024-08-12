e01f72972b6739995df35ab1b00d7f63df48c54b
Sandro-Zosiashvili <zosiashvilisavle@gmail.com>
2024-08-12 19:21:07 +0300

    fix

---
+++ app/components/UserPlaylist/UserPlaylist.tsx
@@ -0,0 +1,70 @@
+'use client'
+import { title } from 'process'
+import styles from './UserPlaylist.module.scss'
+import Image from 'next/image'
+import { useState } from 'react'
+import Link from 'next/link'
+
+
+export default () => {
+    const [active, setActive] = useState(true)
+
+    const onclick = () => {
+        setActive(false)
+        console.log("Active state after click:");
+
+    }
+
+
+    const playListData = [
+        {
+            title: 'Playlist name 1',
+            icon: 'albumicon1.svg',
+            id: 1,
+        }, {
+            title: 'Playlist name 2',
+            icon: 'albumicon2.svg',
+            id: 2,
+        }, {
+            title: 'Playlist name 3',
+            icon: 'albumicon3.svg',
+            id: 3,
+        }, {
+            title: 'Playlist name 4',
+            icon: 'albumicon4.svg',
+            id: 4,
+        }, {
+            title: 'Playlist name 5',
+            icon: 'albumicon5.svg',
+            id: 5
+        }
+    ]
+
+
+    return (
+        <>
+            {
+                playListData.map((item) =>
+                    <Link className={styles.container} href={`playlists/${item.id}`}
+                        onClick={onclick}
+
+                    >
+                        <div className={styles.hoveredImage} >
+                            <Image src={`./icon/${item.icon}`} width={234} height={251} alt='image' />
+                            <div className={styles.buttons}>
+                                <div className={styles.cellEdit}>
+                                    <Image src={'./icon/edit.svg'} width={24} height={24} alt={'edit button'} />
+                                </div>
+                                <div className={styles.cellDelete}>
+                                    <Image src={'./icon/delete.svg'} width={24} height={24} alt={'edit button'} />
+                                </div>
+                            </div>
+                        </div>
+                        <div className={styles.font}>{item.title}</div>
+                    </Link>
+                )
+            }
+        </>
+    )
+}
+
