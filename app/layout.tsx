d7d9eebdc6f93525e6b48cada75dbec0f8929199
Sandro-Zosiashvili <zosiashvilisavle@gmail.com>
2024-08-13 13:50:08 +0300

    fix: global colors

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,12 +20,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <div style={{ display: 'flex'}}>
-          <Menu />
-          {children}
-        </div>
-        <IndexPage />
+      <body className={inter.className}>  
+        {children}
       </body>
     </html>
   );
