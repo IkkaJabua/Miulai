0a42e3f4cbb98938b4907d3c31fdffb17e2eba61
giorgimemarnidze <158182170+giorgimemarnidze@users.noreply.github.com>
2024-08-13 16:22:33 +0400

    feat: created top hit page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 'use client';
 import styles from '../app/page.module.scss';
-import Homepage from './Homepage/page';
+
 
 
 export default function Home() {
   return (
     <main className={styles.main}>
-        <Homepage />
+     
     </main>
   );
 }
