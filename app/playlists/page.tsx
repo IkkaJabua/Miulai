0ef5e294ec67947b5514c2a0798983a0ef0e3571
Sandro-Zosiashvili <zosiashvilisavle@gmail.com>
2024-08-21 02:39:30 +0300

    Update page.tsx

--- app/playlists/page.tsx
+++ app/playlists/page.tsx
@@ -7,10 +7,10 @@ import styles from './page.module.scss'
 import Image from 'next/image';
 import Table from '../components/Table/Table';
 import CreatePlaylist from '../components/Playlist/CreatePlaylist/CreatePlaylist';
+import Link from 'next/link';
 
 
 export default () => {
-
     const [active, setActive] = useState(false)
 
 
@@ -18,7 +18,9 @@ export default () => {
         <div className={styles.container}>
             <div className={styles.miniContainer}>
                 <div className={styles.cellheader}>
-                    <Image className={styles.cursor} src={'./icon/isari.svg'} width={32} height={32} alt='image' />
+                    <Link href={'./'}>
+                        <Image className={styles.cursor} src={'./icon/isari.svg'} width={32} height={32} alt='image' />
+                    </Link>
                     <Image src={'./icon/profile-icon.svg'} width={56} height={56} alt='profile image' />
                 </div>
                 <div className={styles.cellFont}>My Playlists</div>
@@ -31,7 +33,7 @@ export default () => {
                         onClick={() => setActive(!active)}
                     />
                 </div>
-                {active &&  
+                {active &&
                     <div className={styles.newPlaylist}><CreatePlaylist /></div>
                 }
             </div>
