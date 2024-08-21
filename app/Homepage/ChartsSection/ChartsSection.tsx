8f5231fb95f261d9f76dd795715ea3d2ad1ab6e1
giorgimemarnidze <158182170+giorgimemarnidze@users.noreply.github.com>
2024-08-21 14:39:34 +0400

    fix: chartsection container maxwidth

--- app/Homepage/ChartsSection/ChartsSection.tsx
+++ app/Homepage/ChartsSection/ChartsSection.tsx
@@ -2,34 +2,6 @@ import MusicCard from '@/app/components/ MusicCard/ MusicCard';
 import styles from './ChartsSection.module.scss';
 
 
-const ChartsData = [
-    {
-        title: 'Sugar (feat. Francesco)',
-        author: 'By Robin Schulz',
-    },
-    {
-        title: 'Sugar (feat. Francesco)',
-        author: 'By Robin Schulz',
-    },
-    {
-        title: 'Sugar (feat. Francesco)',
-        author: 'By Robin Schulz',
-    },
-    {
-        title: 'Sugar (feat. Francesco)',
-        author: 'By Robin Schulz',
-    },
-    {
-        title: 'Sugar (feat. Francesco)',
-        author: 'By Robin Schulz',
-    },
-    {
-        title: 'Sugar (feat. Francesco)',
-        author: 'By Robin Schulz',
-    },
-]
-
-
 const ChartsSection = () => {
 
     return (
