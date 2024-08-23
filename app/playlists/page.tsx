cb0b19b1e415a7622ddc055b8ffdef78f20b2e7d
Sandro-Zosiashvili <zosiashvilisavle@gmail.com>
2024-08-23 22:57:24 +0300

    Update page.tsx

--- app/playlists/page.tsx
+++ app/playlists/page.tsx
@@ -14,25 +14,54 @@ export default () => {
 
     const [active, setActive] = useState(false)
 
+    const [tablet, setTablet] = useState<any>(false)
+
+    useEffect(() => {
+
+        const mediaQuery = window.matchMedia('(max-width: 744px)');
+        if (window.innerWidth <= 744) {
+            setTablet(mediaQuery.matches);
+        }
+    }, []);
+
 
     return (
         <div className={styles.container}>
             <div className={styles.miniContainer}>
                 <div className={styles.cellheader}>
-                    <Link href={'/'}>
-                        <Image className={styles.cursor} src={'./icon/isari.svg'} width={32} height={32} alt='image' />
-                    </Link>
+                    <div>
+                        <Link href={'/'}>
+                            <Image className={styles.cursor} src={'./icon/isari.svg'} width={32} height={32} alt='image' />
+                        </Link>
+                        <Image className={styles.tabletBurgercursos} src={'../icon/menu-burger.svg'} width={44} height={44} alt='burger-menu-icon' />
+                    </div>
                     <Image src={'./icon/profile-icon.svg'} width={56} height={56} alt='profile image' />
                 </div>
-                <div className={styles.cellFont}>My Playlists</div>
+                <div className={styles.cellFont}>
+                    <Link href={'/'}>
+                        <Image className={styles.tabletCursos} src={'./icon/isari.svg'} width={32} height={32} alt='image' />
+                    </Link>
+                    <div className={styles.mobileGap}>
+                        <div className={styles.cellMy}>
+                            My Playlists
+                        </div>
+                        <div className={styles.mobileButton}>
+                            <Button title={'+'} mode={'reusable width'} width={'28px'} onClick={() => console.log('button clicked')} />
+                        </div>
+                    </div>
+                </div>
                 <div className={styles.cellInput}>
-                    <Input />
-                    <Button
-                        title={'New playlist'}
-                        mode={'short with icon'}
-                        icon
-                        onClick={() => setActive(!active)}
-                    />
+                    <div className={styles.cellReusableinput}>
+                        <Input />
+                    </div>
+                    <div className={styles.screenButton}>
+                        <Button
+                            title={'New playlist'}
+                            mode={'short with icon'}
+                            icon
+                            onClick={() => setActive(!active)}
+                        />
+                    </div>
                 </div>
                 <div className={styles.newPlaylist}>
                     {active &&
