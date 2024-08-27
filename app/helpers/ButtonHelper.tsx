345efeb984631d649a7041927f96b821af784c25
Sandro-Zosiashvili <zosiashvilisavle@gmail.com>
2024-08-28 00:18:33 +0400

    fix: css code

--- app/helpers/ButtonHelper.tsx
+++ app/helpers/ButtonHelper.tsx
@@ -31,26 +31,17 @@ export const getButtonStyles = (props: Props) => {
 
     };
 
+    // ====== names for button icons
+    // 'disabled-clip.svg'
+    // 'disabled-plus.svg'
+    // 'clip.svg'
+    // 'plus.svg'
+
+
     useEffect(() => {
         const newClasses: string[] = [];
-        if (props.disabled && props.mode === 'without icon') {
-            newClasses.push(styles.without_icon_disabled);
-        } else if (props.disabled && props.mode === 'long with icon') {
-            newClasses.push(styles.long_with_icon_disabled);
-            setClip(<Image src='../icon/disabled-clip.svg' alt='clip logo' width={20} height={20} />);
-        } else if (props.disabled && props.mode === 'short with icon') {
-            newClasses.push(styles.short_with_icon_disabled);
-            setClip(<Image src='../icon/disabled-plus.svg' alt='clip logo' width={20} height={20} />);
-        } else if (props.disabled && props.mode === 'reusable button') {
+        if (props.disabled && props.mode === 'reusable button') {
             newClasses.push(styles.reusableWidthDisabled)
-        } else if (props.mode === 'without icon') {
-            newClasses.push(styles.WithoutIcon);
-        } else if (props.mode === 'long with icon') {
-            newClasses.push(styles.LongWithIcon);
-            setClip(<Image src='../icon/clip.svg' alt='clip logo' width={20} height={20} />);
-        } else if (props.mode === 'short with icon') {
-            newClasses.push(styles.ShortWithIcon);
-            setClip(<Image src='../icon/plus.svg' alt='plus' width={20} height={20} />);
         } else if (props.mode === 'reusable button') {
             newClasses.push(styles.reusableButton)
         }
