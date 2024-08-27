f6f17ff3834e101ff2e73aa740269a24e0473993
Sandro-Zosiashvili <zosiashvilisavle@gmail.com>
2024-08-28 00:36:19 +0400

    Update News.tsx

--- app/components/News/News.tsx
+++ app/components/News/News.tsx
@@ -13,7 +13,7 @@ export default (props: Props) => {
         backgroundRepeat: 'no-repeat',
         backgroundSize: 'cover',
         backgroundPosition: 'left',
-        
+
     }
 
     return (
@@ -23,7 +23,18 @@ export default (props: Props) => {
                     <div className={styles.font_style_news}>{props.title}</div>
                     <div className={styles.font_style_plays}>795,900 Plays</div>
                 </div>
-                <Button title={'Listen Now'} mode={'long with icon'} icon onClick={() => console.log('button clicked')} />
+                <Button title={'Listen Now'}
+                    mode={'reusable button'}
+                    padding='12px 24px 12px 20px'
+                    borderRadius='8px'
+                    gap='4px'
+                    width='153px'
+                    fontSize='16px'
+                    fontWeight='500'
+                    imageSrc='clip.svg'
+                    imageWidth={20}
+                    imageHeight={20}
+                    onClick={() => console.log('button clicked')} />
             </div>
         </div>
     )
