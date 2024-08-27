e9a06fdd45f4ed4124f90612e45c70105523f06d
Sandro-Zosiashvili <zosiashvilisavle@gmail.com>
2024-08-28 00:26:50 +0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ import HitsSection from './Homepage/HitsSection/HitsSection';
 export default function Home() {
   return (
     <main className={styles.main}>
-      {/* <Header />
+      <Header />
       <News title={'Top Hit Of The Week'} image={'/image/testImg.jpg'} />
       <CardsHeader title={'Top Hits'} subtitle={''} />
       <HitsSection />
@@ -23,17 +23,7 @@ export default function Home() {
       <CardsHeader title={'Popular Artists'} subtitle={''} />
       <ArtistSection />
       <CardsHeader title={'Popular Albums'} subtitle={''} />
-      <AlbumSection /> */}
-      <Button title={'Listen Now'} 
-      mode='reusable button'
-      width='153px'
-      padding='12px 24px 12px 20px'
-      imageSrc='clip.svg'
-      imageWidth={20}
-      imageHeight={20}
-      borderRadius='8px'
-      gap='4px'
-       onClick={() => console.log('button clicked')} />
+      <AlbumSection />
     </main>
   );
 }
