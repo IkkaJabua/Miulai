a4efeb3283ad4efe7dc5645d0c23308d8bee4a2f
Sandro-Zosiashvili <zosiashvilisavle@gmail.com>
2024-09-06 19:02:50 +0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 import { Butterfly_Kids } from 'next/font/google';
 import styles from '../app/page.module.scss';
-import BurgerMenu from './components/BurgerMenu/BurgerMenu';
 import CardsHeader from './components/CardsHeader/CardsHeader';
 import Header from './components/Header/Header';
 import News from './components/News/News';
@@ -9,7 +8,6 @@ import AlbumSection from './Homepage/AlbumSection/AlbumSection';
 import ArtistSection from './Homepage/ArtistSection/ArtistSection';
 import ChartsSection from './Homepage/ChartsSection/ChartsSection';
 import HitsSection from './Homepage/HitsSection/HitsSection';
-import Mobilemenu from './components/Mobilemenu/mobilemenu';
 
 
 export default function Home() {
