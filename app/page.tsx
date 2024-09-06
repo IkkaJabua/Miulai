924976674c8b320fa0e913bddf11b751140e4000
giorgimemarnidze <158182170+giorgimemarnidze@users.noreply.github.com>
2024-09-06 17:08:44 +0400

    fix: commit something

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+'use client';
+import Header from "./components/Header/Header";
+import Input from "./components/Input/Input";
+import Card from "./components/Card/Card";
+import Heart from "./components/CardItems/CardHeart/Heart";
+import ItemsUnion from "./components/CardItems/ItemsUnion/ItemsUnion";
+import "./page.module.scss";
+import React from "react";
+
+export default function Home() {
+  return (
+    <main>
+        
+    </main>
+  );
+}
