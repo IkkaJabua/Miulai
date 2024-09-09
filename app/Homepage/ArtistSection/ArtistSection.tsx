2eea97fecac596ac99f5a11a943ccd74c27feb53
giorgimemarnidze <158182170+giorgimemarnidze@users.noreply.github.com>
2024-09-09 17:41:01 +0400

    Fixed/Build-errors
    Errors resolving

--- app/Homepage/ArtistSection/ArtistSection.tsx
+++ app/Homepage/ArtistSection/ArtistSection.tsx
@@ -5,26 +5,31 @@ const ArtistData = [
     {
         image: '/image/swift.png',
         title: 'Taylor Swift',
+        id: 1
     },
 
     {
         image: '/image/eilish.png',
         title: 'Billie Eilish',
+        id: 2
     },
 
     {
         image: '/image/sza.png',
         title: 'SZA',
+        id: 3
     },
 
     {
         image: '/image/test.png',
         title: 'Ed Sheeran',
+        id: 4
     },
 
     {
         image: '/image/test.png',
         title: 'Ed Sheeran',
+        id: 5
     },
 ]
 
@@ -35,8 +40,8 @@ const ArtistSection = () => {
             <div className={styles.art}>
                 {
                     ArtistData.map((item, i) => (
-                        <div className={styles.box}>
-                            <Card header={''} image={item.image} title={item.title} imageStyle={'round'} />
+                        <div className={styles.box} key={item.id}>
+                            <Card header={''} key={item.id} image={item.image} title={item.title} imageStyle={'round'} />
                         </div>
                     ))
                 }
--- app/Homepage/HitsSection/HitsSection.tsx
+++ app/Homepage/HitsSection/HitsSection.tsx
@@ -6,30 +6,35 @@ const hitsData = [
         image: '/image/test.png',
         subtitle: 'Anyma',
         title: 'Genesys ||',
+        id: 1
     },
 
     {
         image: '/image/test.png',
         subtitle: 'Anyma',
         title: 'Genesys ||',
+        id: 2
     },
 
     {
         image: '/image/test.png',
         subtitle: 'Anyma',
         title: 'Genesys ||',
+        id: 3
     },
 
     {
         image: '/image/test.png',
         subtitle: 'Anyma',
         title: 'Genesys ||',
+        id: 4
     },
 
     {
         image: '/image/test.png',
         subtitle: 'Anyma',
         title: 'Genesys ||',
+        id: 5
     },
 ]
 
@@ -40,8 +45,8 @@ const HitsSection = () => {
             <div className={styles.hits}>
                 {
                     hitsData.map((item, i) => (
-                        <div className={styles.box}>
-                            <Card header={''} image={item.image} subtitle={item.subtitle} title={item.title} imageStyle={'normal'} />
+                        <div className={styles.box} key={item.id}>
+                            <Card header={''} key={item.id} image={item.image} subtitle={item.subtitle} title={item.title} imageStyle={'normal'} />
                         </div>
                     ))
                 }
