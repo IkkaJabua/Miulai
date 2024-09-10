b34843a51c0b4b4753820c9c3951089f35b0c96e
IkkaJabua <156788844+IkkaJabua@users.noreply.github.com>
2024-09-10 17:02:18 +0400

    fix: file name

--- app/components/Musicplayer/Trackdisplay.tsx
+++ app/components/Musicplayer/Trackdisplay.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Image from 'next/image'
 import Button from '../Button/Button';
-import style from './Trackdisplay.module.scss';
+import style from './TrackDisplay.module.scss';
 import HeartShapeBtn from '../heatShapeIcon/HeartShapeIcn';
 
 interface TrackDisplayProps {
