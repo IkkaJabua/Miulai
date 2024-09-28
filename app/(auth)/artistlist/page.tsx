8ece3e9e1d5472fe9668f71bb93daeaa9ffcf9c0
IkkaJabua <156788844+IkkaJabua@users.noreply.github.com>
2024-09-28 17:43:00 +0400

    build eror

--- app/(auth)/artistlist/page.tsx
+++ app/(auth)/artistlist/page.tsx
@@ -1,4 +1,5 @@
-'use client';
+"use client"; // Add this line to mark the component as a Client Component
+
 import styles from './page.module.scss';
 import { useRouter } from 'next/navigation';
 import Header from '@/app/components/Header/Header';
@@ -8,43 +9,46 @@ import axios from 'axios';
 import { useRecoilState } from 'recoil';
 import { albumidState } from '@/app/state';
 
-    
-
 const ArtistsList = () => {
-    const [albumId, setAlbumId] = useRecoilState(albumidState);
+    const [, setAlbumId] = useRecoilState(albumidState);
     const router = useRouter();
+
     const handleCardClick = () => {
-        axios.get(`https://interstellar-1-pdzj.onrender.com/author`)
-        
         router.push(`/artist`);
     };
-
-
 
     const [artists, setArtists] = useState([]);
 
     useEffect(() => {
         axios.get(`https://interstellar-1-pdzj.onrender.com/author`)
-        .then((r) => {
-            setArtists(r.data)
-        })
-        
-    },[])
+            .then((r) => {
+                setArtists(r.data);
+            })
+            .catch((error) => {
+                console.error("Error fetching artists:", error);
+            });
+    }, []);
 
     return (
         <div className={styles.container}>
             <Header />
             <div className={styles.container2}>
                 <h2 className={styles.h2}>Trending Now</h2>
-
                 <div className={styles.wrapper}>
                     {
-                        artists.map((item: any, i) => (
-                            <div onClick={() => {
-                                setAlbumId(item.id)
-                                handleCardClick()
-                            }} >   
-                                <Card key={i} image={item?.files[0]?.url} title={item.firstName} imageStyle={'round'} />
+                        artists.map((item: any) => (
+                            <div
+                                key={item.id} // Assign the unique key here
+                                onClick={() => {
+                                    setAlbumId(item.id);
+                                    handleCardClick();
+                                }} 
+                            >
+                                <Card
+                                    image={item?.files[0]?.url}
+                                    title={item.firstName}
+                                    imageStyle={'round'}
+                                />
                             </div>
                         ))
                     }
