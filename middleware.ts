a3b20a8d23c4f61d6d3114c2ca65c2fe3bde53db
Sandro-Zosiashvili <zosiashvilisavle@gmail.com>
2024-10-01 02:10:55 +0400

    fix: middelwear

--- middleware.ts
+++ middleware.ts
@@ -1,31 +1,24 @@
 import { NextRequest, NextResponse } from "next/server";
 import { cookies } from "next/headers";
 
-
-const publicRoutes = ['/signin', '/signup']
+const publicRoutes = ['/signin', '/signup', '/']; 
 
 export default async function middleware(req: NextRequest) {
-    // NextResponse.redirect('/')
-    // console.log(cookies().get('token'));
-    // console.log('ddd');
-
     const token = cookies().get('token');
-    const path = req.nextUrl.pathname
-    
-    const pathIsPublic = publicRoutes.includes(path)
+    const path = req.nextUrl.pathname; 
+    const pathIsPublic = publicRoutes.includes(path); 
+    console.log({ token, path, pathIsPublic });
 
-    if(/*path === '/signin'*/ pathIsPublic && token) { 
-      return  NextResponse.redirect(new URL('/', req.url))
+    if (pathIsPublic && token) {
+        return NextResponse.redirect(new URL('/', req.url));
     }
 
-    if(!token && !pathIsPublic /*path !== '/signin'*/) {
-      
-        return NextResponse.redirect(new URL('/signup', req.url))
+    if (!token && !pathIsPublic) {
+        return NextResponse.redirect(new URL('/signup', req.url));
     }
 
-    return NextResponse.next()
+    return NextResponse.next();
 }
-
 export const config = {
     matcher: ['/((?!api|_next/static|_next/image|favicon.ico|image|icon|.png$).*)'],
-} 
+};
