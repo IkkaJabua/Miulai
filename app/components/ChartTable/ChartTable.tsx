04b093e690e6ee48dd72b46ea8236ec7b75c5049
Sandro-Zosiashvili <zosiashvilisavle@gmail.com>
2024-10-01 01:34:42 +0400

    fix: errors

--- app/components/ChartTable/ChartTable.tsx
+++ app/components/ChartTable/ChartTable.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { Table } from "antd";
-import HeartShapeBtn from "../../heatShapeIcon/HeartShapeIcn";
+import HeartShapeBtn from "../heatShapeIcon/HeartShapeIcn";
 import type { DividerClassKey } from "@mui/material";
 import styles from "./ChartTable.module.scss";
 import { render } from "sass";
