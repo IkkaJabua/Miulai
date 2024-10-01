0e3f861e5d3670cadc2a48cb705c5a9d7f718d42
IkkaJabua <156788844+IkkaJabua@users.noreply.github.com>
2024-10-02 03:59:11 +0400

    test

---
+++ app/components/MusicPlayer/fullScreen.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import Image from 'next/image'
+import Button from '../Button/Button';
+import style from './fullscreen.module.scss';
+import HeartShapeBtn from '../heatShapeIcon/HeartShapeIcn';
+import { useRecoilState } from 'recoil';
+import { musicState } from '@/app/state';
+
+const Fullscreen = () => {
+    const [music, setMusic] = useRecoilState<any>(musicState)
+
+    return (
+        <div className={style.container}>
+            <img src={music?.coverImgUrl || '/defaultAlbumArt.jpg'} alt="Album Art" width={80} height={80} className={style.img} />
+            <div className={style.like}>
+                <div className={style.text}>
+                    <div className={style.flexing}>
+                        <span className={style.title}>{music?.title || 'Unknown Title'}</span>
+                        <span className={style.artist}>{music?.artistName || 'Unknown Artist'}</span>
+                        <div className={style.likebtn}>
+                        </div></div>
+                </div>
+
+            </div>
+
+        </div>
+    );
+};
+
+export default Fullscreen;
